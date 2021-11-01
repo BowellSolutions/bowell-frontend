21dd64dfe29f2da98f8d362b007aaf2d6a60e0b2
Alschn <adam.lisichin@gmail.com>
2021-11-01 17:05:10 +0100

    Install msw package and cypress utils
    Added msw package for mocking api calls, cypress-msw-interceptor (to be configured), @testing-library/cypress for additional commands. Wrote e2e test for login page.

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('Test login page', () => {
+  it('submits login form and succeeds', () => {
+    cy.visit('/login');
+    // verify should fail because user is not authenticated
+    cy.intercept('/api/auth/token/verify/', {
+      statusCode: 400,
+      body: {}
+    });
+    // fill in the form and submit
+    cy.get('form').within(() => {
+      cy.get('input[name="email"]').type('test');
+      cy.get('input[name="password"]').type('password');
+      cy.get('button').click();
+    });
+    // mock api response
+    cy.intercept('/api/auth/token/', 'success');
+    cy.intercept('/api/users/me', {
+      statusCode: 200,
+      body: {
+        id: 1,
+        username: 'test',
+        email: 'test@gmail.com',
+        first_name: 'test',
+        last_name: 'test',
+      }
+    });
+    // successful authentication
+    cy.intercept('/api/auth/token/verify/', 'success');
+    // redirect to dashboard
+    cy.url({timeout: 10000}).should('eq', 'http://localhost:3000/dashboard');
+  });
+});
