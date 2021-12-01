3a6ec188dda1f5608e4acfe5ff503a18b2168da0
Alschn <adam.lisichin@gmail.com>
2021-12-01 22:23:18 +0100

    BS-52 Fix some cypress tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,22 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Test login page', () => {
-  beforeEach(() => cy.visit('/login'));
-
   it('submits login form and succeeds', () => {
+    cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+    cy.visit('/login');
     // verify should fail because user is not authenticated
-    cy.intercept('/api/auth/token/verify/', {
-      statusCode: 400,
-      body: {}
-    });
-    // fill in the form and submit
-    cy.get('form').within(() => {
-      cy.get('input[name="email"]').type('test');
-      cy.get('input[name="password"]').type('password');
-      cy.get('button').click();
-    });
+    cy.wait('@verifyFail', {timeout: 10_000});
     // mock api response
-    cy.intercept('/api/auth/token/', 'success');
+    cy.intercept('/api/auth/token/', 'success').as('verifyAfterLogin');
     cy.intercept('/api/users/me', {
       statusCode: 200,
       body: {
@@ -27,22 +19,31 @@ describe('Test login page', () => {
         last_name: 'test',
         is_staff: true,
       }
+    }).as('getUser');
+    // fill in the form and submit
+    cy.get('form').within(() => {
+      cy.get('input[name="email"]').type('test');
+      cy.get('input[name="password"]').type('password');
+      cy.get('button').click();
     });
     // successful authentication
-    cy.intercept('/api/auth/token/verify/', 'success');
+    cy.wait('@verifyAfterLogin', {timeout: 10_000});
+    cy.wait('@getUser', {timeout: 10_000});
     // redirect to dashboard
     cy.url({timeout: 20000}).should('eq', 'http://localhost:3000/dashboard');
   });
 
   it('tries to submit but fails validation', () => {
+    cy.visit('/login');
     // to do
   });
 
   it('submits login form and fails', () => {
+    cy.visit('/login');
     // to do
   });
 
   it('redirects logged user to dashboard', () => {
-    // to do
+    cy.visit('/login');
   });
 });
--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,19 +1,21 @@
 describe('Test register page', () => {
-  beforeEach(() => cy.visit("/register"));
-
   it('submits form and succeeds', () => {
+    cy.visit("/register");
     // to do
   });
 
   it('tries to submit but fails validation', () => {
+    cy.visit("/register");
     // to do
   });
 
   it('submits register form and fails', () => {
+    cy.visit("/register");
     // to do
   });
 
   it('redirects logged user to dashboard', () => {
+    cy.visit("/register");
     // to do
   });
 });
