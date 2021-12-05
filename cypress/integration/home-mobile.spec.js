873712cbeafeb77f1c8dc5bf163d484ee9ce264b
Alschn <adam.lisichin@gmail.com>
2021-12-05 17:51:17 +0100

    BS-73 Test home page mobile view with Cypress

---
+++ cypress/integration/home-mobile.spec.js
@@ -0,0 +1,148 @@
+/// <reference types="cypress" />
+
+
+describe('test homepage - mobile features only', () => {
+  context('average mobile phone resolution', () => {
+    beforeEach(() => {
+      cy.viewport(360, 640);
+    });
+
+    it('clicks on hamburger menu', () => {
+      cy.intercept('/api/auth/token/verify/', 'success');
+      cy.visit('/');
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').should('be.visible');
+      });
+    });
+
+    it('clicks on hamburger menu, then Home', () => {
+      cy.visit('/');
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Home').click()
+          .then(
+            () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/')
+          );
+      });
+    });
+
+    it('clicks on hamburger menu, then Dashboard - user not logged in', () => {
+      cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+      cy.visit('/');
+      cy.wait('@verifyFail', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Dashboard').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/login')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Dashboard - user logged in', () => {
+      cy.intercept('/api/auth/token/verify/', 'success').as('verifySuccess');
+
+      cy.visit('/');
+      cy.wait('@verifySuccess', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Dashboard').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/dashboard')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Examinations - user not logged in', () => {
+      cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+      cy.visit('/');
+      cy.wait('@verifyFail', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Examinations').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/login')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Examinations - user logged in', () => {
+      cy.intercept('/api/auth/token/verify/', 'success').as('verifySuccess');
+
+      cy.visit('/');
+      cy.wait('@verifySuccess', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Examinations').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/dashboard/examinations')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Recordings - user not logged in', () => {
+      cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+      cy.visit('/');
+      cy.wait('@verifyFail', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Recordings').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/login')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Recordings - user logged in', () => {
+      cy.intercept('/api/auth/token/verify/', 'success').as('verifySuccess');
+
+      cy.visit('/');
+      cy.wait('@verifySuccess', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Recordings').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/dashboard/recordings')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Sign In - user not logged in', () => {
+      cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+      cy.visit('/');
+      cy.wait('@verifyFail', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Sign in').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/login')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Sign Up - user not logged in', () => {
+      cy.intercept('/api/auth/token/verify/', {statusCode: 400}).as('verifyFail');
+
+      cy.visit('/');
+      cy.wait('@verifyFail', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Sign up').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/register')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Profile - user logged in', () => {
+      cy.intercept('/api/auth/token/verify/', 'success').as('verifySuccess');
+
+      cy.visit('/');
+      cy.wait('@verifySuccess', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Profile').click().then(
+          () => cy.url({timeout: 20_000}).should('eq', 'http://localhost:3000/dashboard/profile')
+        );
+      });
+    });
+
+    it('clicks on hamburger menu, then Logout - user logged in', () => {
+      cy.intercept('/api/auth/token/verify/', 'success').as('verifySuccess');
+      cy.intercept('/api/auth/logout/', 'success').as('logoutSuccess');
+
+      cy.visit('/');
+      cy.wait('@verifySuccess', {timeout: 10_000});
+      cy.get('#hamburger-menu').click().then(() => {
+        cy.get('#mobile-nav').find('button').contains('Logout').click();
+        cy.wait('@logoutSuccess', {timeout: 10_000});
+      });
+    });
+  });
+});
