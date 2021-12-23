57cf71bd7f7452e3cfd6af8ea46fc25f4ab13f4d
Alschn <adam.lisichin@gmail.com>
2021-12-24 00:15:07 +0100

    BS-87 Add simple search filter in patients
    Added simple search filter in patients view using custom useTableFilter hook

--- src/components/views/doctor/Patients.tsx
+++ src/components/views/doctor/Patients.tsx
@@ -1,5 +1,19 @@
-import {FC, useEffect} from "react";
-import {Flex, Table, Tbody, Text, Th, Thead, Tr, useColorModeValue} from "@chakra-ui/react";
+import {FC, useEffect, useState} from "react";
+import {
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Table,
+  Tbody,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue
+} from "@chakra-ui/react";
+import {AiOutlineSearch} from "react-icons/ai";
 import CardBody from "../../card/CardBody";
 import CardHeader from "../../card/CardHeader";
 import PatientsTableRow from "../../tables/PatientsTableRow";
@@ -7,17 +21,28 @@ import Card from "../../card/Card";
 import {useAppSelector} from "../../../redux/hooks";
 import {useDispatch} from "react-redux";
 import {loadPatients} from "../../../redux/actions/dashboard";
+import useTableFilter from "../../../hooks/useTableFilter";
 
 
 const Patients: FC = () => {
   const textColor = useColorModeValue("gray.700", "white");
 
   const dispatch = useDispatch();
-  const patients = useAppSelector(state => state.dashboard.patients.filter(p => p.type === "PATIENT"));
+  const patients = useAppSelector(
+    state => state.dashboard.patients.filter(p => p.type === "PATIENT" && p.id !== user?.id)
+  );
   const user = useAppSelector(state => state.auth.user);
 
+  const [query, setQuery] = useState<string>(""); // rows will be filtered by this query
+
+  const filteredPatients = useTableFilter(
+    patients,
+    ["id", "first_name", "last_name", "email"],
+    query
+  );
+
   useEffect(() => {
-    // try to load patients on each load because they could have changed
+    // load patients on each load because they could have changed
     dispatch(loadPatients());
   }, [dispatch]);
 
@@ -29,6 +54,23 @@ const Patients: FC = () => {
             <Text fontSize="xl" color={textColor} fontWeight="bold">
               Patients
             </Text>
+          </Flex>
+
+          <Flex>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={AiOutlineSearch}/>
+              </InputLeftElement>
+
+              <Input
+                id="patients-search-field"
+                name="search"
+                type="search"
+                size="md"
+                placeholder="Search"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </InputGroup>
           </Flex>
         </CardHeader>
 
@@ -48,7 +90,7 @@ const Patients: FC = () => {
             </Thead>
 
             <Tbody>
-              {patients.filter(p => p.id !== user?.id).map((user) => (
+              {filteredPatients.map((user) => (
                 <PatientsTableRow
                   id={user.id}
                   firstName={user.first_name}
