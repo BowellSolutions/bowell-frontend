81283a180c3ddf44596564149204d38c7cdfb145
Alschn <adam.lisichin@gmail.com>
2022-01-04 12:13:34 +0100

    BS-100 Setup working websocket client
    Created WebsocketContextProvider hoc, which wraps around Component in _app, connects to ws backend if conditions are satisfied and passes values to context so that any component in dashboard can access them.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import type {AppProps} from 'next/app';
 import {wrapper} from "../redux/store";
 import {ChakraProvider} from "@chakra-ui/react";
 import theme from "../theme/theme";
+import WebsocketContextProvider from "../components/context/WebsocketContext";
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      <Component {...pageProps} />
+      <WebsocketContextProvider>
+        <Component {...pageProps} />
+      </WebsocketContextProvider>
     </ChakraProvider>
   );
 }
