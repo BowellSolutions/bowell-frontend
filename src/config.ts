54948fe441a7de17856bef792f85a1532757f599
Alschn <adam.lisichin@gmail.com>
2022-01-04 17:07:51 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export const ACCESS_TOKEN_LIFETIME = publicRuntimeConfig.API_ACCESS_TOKEN_LIFETI
 
 const API_URL_OBJECT = new URL(API_URL);
 
-export const WS_SCHEME = process.env.NODE_ENV === "production" ? 'wss' : 'ws';
+export const WS_SCHEME = API_URL_OBJECT.protocol === "https:" ? 'wss' : 'ws';
 export const API_HOST = process.env.NODE_ENV === "production" ? API_URL_OBJECT.host : '127.0.0.1:8000';
