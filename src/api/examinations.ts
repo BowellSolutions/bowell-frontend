237672e2f70ed1bcf2c930167f4a9c9481394766
Alschn <adam.lisichin@gmail.com>
2022-01-16 22:58:08 +0100

    BS-106 Add comments to api axios utils

--- src/api/examinations.ts
+++ src/api/examinations.ts
@@ -1,14 +1,7 @@
-/*
-* @author: Adam Lisichin
-* @file: Exports examination related functions performing HTTP calls with axiosClient
-* Available functions:
-* - getExaminations
-* - getExamination
-* - createExamination
-* - updateExamination
-* - startInference
-* - getInferenceState
-*/
+/**
+ * @author: Adam Lisichin
+ * @file: Exports examination related functions performing HTTP calls with axiosClient
+ */
 import AxiosClient from "./axiosClient";
 import {
   CreateExaminationData,
@@ -20,20 +13,32 @@ import {
 } from "./types";
 import {AxiosRequestConfig} from "axios";
 
+/**
+ Sends GET request at /api/examinations/
+ */
 export const getExaminations = (options?: AxiosRequestConfig): Promise<PaginatedResponse<ExaminationData>> => {
   return AxiosClient.get('/examinations/', {...options});
 };
 
+/**
+ Sends GET request at /api/examinations/[id]/
+ */
 export const getExamination =
   (examinationID: number | string, options?: AxiosRequestConfig): Promise<Response<ExaminationData>> => {
     return AxiosClient.get(`/examinations/${examinationID}/`, {...options});
   };
 
+/**
+ Sends POST request at /api/examinations/[id]/
+ **/
 export const createExamination =
   (data: CreateExaminationData, options?: AxiosRequestConfig): Promise<Response<ExaminationData>> => {
     return AxiosClient.post('/examinations/', {...data}, {...options});
   };
 
+/**
+ Sends PATCH request at /api/examinations/[id]/
+ */
 export const updateExamination =
   (
     examinationID: number | string,
@@ -43,11 +48,17 @@ export const updateExamination =
     return AxiosClient.patch(`/examinations/${examinationID}/`, {...data}, {...options});
   };
 
+/**
+ Sends POST request at /api/examinations/[id]/inference/
+ */
 export const startInference = (examinationID: number | string, options?: AxiosRequestConfig)
-  : Promise<Response<{message: string}>> => {
+  : Promise<Response<{ message: string }>> => {
   return AxiosClient.post(`/examinations/${examinationID}/inference/`, {}, {...options});
 };
 
+/**
+ Sends GET request at /api/examinations/[id]/inference/
+ */
 export const getInferenceState = (examinationID: number | string, options?: AxiosRequestConfig)
   : Promise<Response<InferenceResults>> => {
   return AxiosClient.get(`/examinations/${examinationID}/inference/`, {...options});
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,19 +1,22 @@
-/*
-* @author: Adam Lisichin
-* @file: Exports user related functions performing HTTP calls with axiosClient
-* Available functions:
-* - getPatients
-* - getDoctors
-*/
+/**
+ * @author: Adam Lisichin
+ * @file: Exports user related functions performing HTTP calls with axiosClient
+ */
 import AxiosClient from "./axiosClient";
 import {PaginatedResponse, UserData} from "./types";
 import {AxiosRequestConfig} from "axios";
 
+/**
+ * Sends GET request at /api/users/?type=PATIENT
+ */
 export const getPatients = (options?: AxiosRequestConfig): Promise<PaginatedResponse<UserData>> => {
   return AxiosClient.get('/users/?type=PATIENT', {...options});
 };
 
 
+/**
+ * Sends GET request at /api/users/?type=DOCTOR
+ */
 export const getDoctors = (options?: AxiosRequestConfig): Promise<PaginatedResponse<UserData>> => {
   return AxiosClient.get('/users/?type=DOCTOR', {...options});
 };
